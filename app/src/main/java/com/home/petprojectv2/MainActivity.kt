dd113687f92f2fc2302684e62aa3ee336eb1625b
Yevhenii Lidovskyi <zhenya.l@hotmail.com>
2025-09-13 18:41:54 +0300

    Updated dependency

--- app/src/main/java/com/home/petprojectv2/MainActivity.kt
+++ app/src/main/java/com/home/petprojectv2/MainActivity.kt
@@ -12,7 +12,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.home.petprojectv2.ui.theme.PetProjectV2Theme
-import java.util.Locale
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
